a936d386e02e589ec4c0e353f7e433f604bf3c6a
Shivang Sharma <13shivangsharma@gmail.com>
2015-07-02 19:28:26 +0530

    a

--- New_modules/Client2706/app/src/main/java/com/example/anonymous/client2706/MyWidgetIntentReceiver.java
+++ New_modules/Client2706/app/src/main/java/com/example/anonymous/client2706/MyWidgetIntentReceiver.java
@@ -14,9 +14,7 @@ public class MyWidgetIntentReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "in on recieve",Toast.LENGTH_SHORT).show();
         if(intent.getAction().equals("pl.looksok.intent.action.CHANGE_PICTURE")){
-            Toast.makeText(context, "in on recieve",Toast.LENGTH_SHORT).show();
             updateWidgetPictureAndButtonListener(context);
         }
     }
@@ -24,18 +22,12 @@ public class MyWidgetIntentReceiver extends BroadcastReceiver {
     private void updateWidgetPictureAndButtonListener(Context context) {
         Toast.makeText(context, "u clicked",Toast.LENGTH_SHORT).show();
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-        remoteViews.setString(R.id.widget_button, "setText", "hey");
 
-        //remoteViews.setImageViewResource(R.id.widget_image, getImageToSet(,));
+         //code to be entered
 
-        //REMEMBER TO ALWAYS REFRESH YOUR BUTTON CLICK LISTENERS!!!
+
         remoteViews.setOnClickPendingIntent(R.id.widget_button, widget.buildButtonPendingIntent(context));
-
         widget.pushWidgetUpdate(context.getApplicationContext(), remoteViews);
     }
 
-    //private int getImageToSet() {
-    //    clickCount++;
-    // return clickCount % 2 == 0 ? R.drawable.me : R.drawable.wordpress_icon;
-    // }
 }
