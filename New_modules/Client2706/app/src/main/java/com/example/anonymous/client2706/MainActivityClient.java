3253413585f08e9380f1f428d32d15ec97b0b4d2
wolf00 <ladrahul00@gmail.com>
2015-07-12 10:21:21 +0530

    Circular progress bar included

--- New_modules/Client2706/app/src/main/java/com/example/anonymous/client2706/MainActivityClient.java
+++ New_modules/Client2706/app/src/main/java/com/example/anonymous/client2706/MainActivityClient.java
@@ -27,13 +27,12 @@ public class MainActivityClient extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_client);
-
-        spinner.setVisibility(View.VISIBLE);
-
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "ecNYEdsTREI9Mwzx5gWOoh2HB9V78KvVWe8W8iIA", "YHuKHkJdjm4gSdl6lrZavY9Sdx06Da1DPNNXy40p");
         SharedPreferences pref = getApplicationContext().getSharedPreferences("mypref" , 0); //0 for private mode
         SharedPreferences.Editor editor = pref.edit();
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
 
         String empID = pref.getString("EmployeeIDKey", "blank");//receive from preference
         editor.commit();
@@ -43,26 +42,19 @@ public class MainActivityClient extends ActionBarActivity {
             intent.putExtra("EmployeeID", empID);
             startActivity(intent);
         }
-    }
+        else{
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            String empMac = bluetoothAdapter.getAddress();
 
-    public void loginCheck(View v){
-        EditText empId = (EditText)findViewById(R.id.employeeid);
-        final String empidString = empId.getText().toString();
-        EditText passwd = (EditText)findViewById(R.id.password);
-        final String passcheck = passwd.getText().toString();
-        BluetoothAdapter myBluetoothAdapter;
-        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        final String macDevice = myBluetoothAdapter.getAddress();
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("EmployeeData");
-        query.whereEqualTo("EmployeeID",empidString);
-        query.getFirstInBackground(new GetCallback<ParseObject>() {
-            @Override
-            public void done(ParseObject parseObject, ParseException e) {
-                if (parseObject == null) {
-                    Log.d("score", "The getFirst request failed.");
-                } else {
-                    if (passcheck.equals(parseObject.get("Password").toString())) {
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("EmployeeData");
+            query.whereEqualTo("MacAddress",empMac);
+            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject parseObject, ParseException e) {
+                    if (parseObject == null) {
+                        Log.d("score", "The getFirst request failed.");
+                    } else {
+                        String empidString=parseObject.get("EmployeeID").toString();
                         SharedPreferences pref = getApplicationContext().getSharedPreferences("mypref", 0); //0 for private mode
                         SharedPreferences.Editor editor = pref.edit();
 
@@ -70,16 +62,10 @@ public class MainActivityClient extends ActionBarActivity {
                         editor.commit();
 
                         Intent intent = new Intent(MainActivityClient.this, CheckInOut.class);
-                        intent.putExtra("EmployeeID", empidString);
                         startActivity(intent);
-                    } else {
-                        TextView errtxt = (TextView) findViewById(R.id.errorText);
-                        errtxt.setText("Password Incorrect!!!");
-                        errtxt.setVisibility(View.VISIBLE);
                     }
                 }
-            }
-        });
+            });
+        }
     }
-
 }
